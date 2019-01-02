ee1250001b65951e728a3e0131c02592bb366747
Harshit Jain <harshitjain6751@gmail.com>
2019-01-02 15:11:56 +0530

    dubai: Strip off some debug packages
    * Ref: https://source.android.com/devices/tech/dalvik/configure#compilation_options
    
    Q: PRODUCT_MINIMIZE_JAVA_DEBUG_INFO wth does this do?
    A: https://android.googlesource.com/platform/build/+/master/target/product/go_defaults_common.mk#68
    Signed-off-by: B4579 <B4579@protonmail.com>

--- device.mk
+++ device.mk
@@ -48,6 +48,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.cp_system_other_odex=1
 
+# ART Debugging (Disable)
+USE_DEX2OAT_DEBUG := false
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt \
