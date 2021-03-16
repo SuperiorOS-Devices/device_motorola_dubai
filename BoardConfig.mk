7ab24ba1d27d4ddace74a4091c5d4367e45bbaa0
Pig <pig.priv@gmail.com>
2021-03-16 19:57:23 +0300

    dubai: Move DPM feature property to /system_ext
    * DPM blobs are under /system_ext.
    
    Signed-off-by: B4579 <B4579@protonmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -172,6 +172,7 @@ TARGET_ODM_PROP += $(DEVICE_PATH)/configs/props/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/props/product.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/configs/props/system_ext.prop
 
 # Recovery
 BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
