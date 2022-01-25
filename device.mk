8b7edf9c30ce63ddffb7756b69ddbaf9c7cd7f67
Josh Wu <joshwu@google.com>
2022-01-24 22:56:14 -0800

    dubai: Switch BtAudio to AIDL
    Test: m
    Bug: 203490261
    Change-Id: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7
    Merged-In: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7
    Signed-off-by: B4579 <B4579@protonmail.com>

--- device.mk
+++ device.mk
@@ -85,7 +85,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.soundtrigger@2.3-impl \
     audioadsprpcd \
     audio.bluetooth.default \
@@ -129,6 +128,7 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth.audio-impl \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
