b09a9ff9d5aaff84ca056a41454f7c7d868a7983
AdarshGrewal <adarshgrewal@gmail.com>
2022-09-24 02:32:40 +0530

    dubai: Drop authsecret service
    Change-Id: Ia84623464f1df8a3d7046c721e83682d1d774cd3
    Signed-off-by: B4579 <B4579@protonmail.com>

--- device.mk
+++ device.mk
@@ -121,10 +121,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration_7_0.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
-    
-# Authsecret
-PRODUCT_PACKAGES += \
-    android.hardware.authsecret@1.0.vendor
 
 # Bluetooth
 PRODUCT_PACKAGES += \
