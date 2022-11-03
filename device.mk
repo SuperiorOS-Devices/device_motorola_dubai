19afb94a171ac3b7651cb7e6e1e61546e1cc8b15
johnmart19 <johnivan19999@gmail.com>
2022-11-03 21:35:01 +0200

    dubai: Nuke OTA Preopt Script
    - OTA Dexopt optimizations taking a lot of time, let's skip it on installation moment
    
    Signed-off-by: B4579 <B4579@protonmail.com>

--- device.mk
+++ device.mk
@@ -30,20 +30,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 TARGET_IS_VAB := true
 
 AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_vendor=true \
     POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
     FILESYSTEM_TYPE_vendor=ext4 \
     POSTINSTALL_OPTIONAL_vendor=true
 
 PRODUCT_PACKAGES += \
-    checkpoint_gc \
-    otapreopt_script
+    checkpoint_gc
 
 # Additional native libraries
 PRODUCT_COPY_FILES += \
